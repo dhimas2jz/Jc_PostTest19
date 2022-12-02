9b139fadcebc7dcc801add23688d2ca4dad5eaa0
hekilledhim <dhimzmarwahz@gmail.com>
2022-12-02 21:05:03 +0700

    first

---
+++ src/test/java/com/dhimas/belajar/belajarcucumber2/PostTest19_1.java
@@ -0,0 +1,13 @@
+package com.dhimas.belajar.belajarcucumber2;
+
+	import io.cucumber.testng.AbstractTestNGCucumberTests;
+	import io.cucumber.testng.CucumberOptions;
+
+	@CucumberOptions (
+			features = "src/test/java/com/dhimas/belajar/belajarcucumber2",
+			glue=""
+		)
+
+	public class PostTest19_1 extends AbstractTestNGCucumberTests {
+
+	}
